4de125f8a5c042fdeb0146096d8d885b1f881141
migodlcrz <lorenzo.cap.delacruz@email.com>
2025-05-22 01:54:42 +0800

    Created Login with creds action

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,6 +1,40 @@
+"use server";
+
+import { signIn } from "@/auth";
 import client from "@/lib/db";
 import { User } from "@/models/User";
 import { saltAndHashPassword } from "@/utils/password";
+import { AuthError } from "next-auth";
+import { revalidatePath } from "next/cache";
+
+export const loginWithCreds = async (formData: FormData) => {
+  const rawFormData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+    redirectTo: "/home",
+  };
+
+  if (!rawFormData.email || !rawFormData.password) {
+    return { error: "All fields are required!" };
+  }
+
+  try {
+    await signIn("credentials", rawFormData);
+    revalidatePath("/");
+    return { success: true };
+  } catch (error: any) {
+    if (error instanceof AuthError) {
+      console.log(error.type);
+      switch (error.type) {
+        case "CallbackRouteError":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
+};
 
 export const registerWithCreds = async (formData: FormData) => {
   const rawFormData = {
